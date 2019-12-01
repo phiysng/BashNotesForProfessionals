282c6e83374a445bb57b3e6649cf911cd30ce3f0
Wu Yuansou <wuyuanshou@protonmail.com>
2019-12-01 15:59:39 +0800

    read and quote usage

--- hello-world.sh
+++ hello-world.sh
@@ -13,3 +13,12 @@ printf "Hello %s\n" "$whom_variable"
 # $1 指 这个bash脚本运行时的第一个参数 ""将这个参数转换为字符串常量
 # 注意这里使用双引号
 printf "Hello %s\n" "$1"
+
+# read
+echo "Who are you"
+read name
+echo "Hello $name."
+
+# append strings.
+read action
+echo "You are ${action}ing."
